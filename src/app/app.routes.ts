f0892ed3f6cadbc335369625350fccab0732485a
Carlos Mazzarri <cjmazzarri@gmail.com>
2024-09-06 15:05:29 -0500

    ✨🚚Added cosmetics component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CosmeticsComponent } from '../components/cosmetics/cosmetics.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: CosmeticsComponent
+    }
+];
---
+++ src/components/cosmetics/cosmetics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cosmetics',
+  standalone: true,
+  imports: [],
+  templateUrl: './cosmetics.component.html',
+  styleUrl: './cosmetics.component.css'
+})
+export class CosmeticsComponent {
+
+}
