2df3b044508dd9d50d33e5b9a85723876d252846
Carlos Mazzarri <cjmazzarri@gmail.com>
2025-01-17 17:00:20 -0500

    ♻️Big refactor into Utils

--- src/components/cosmetic-list/cosmetic-list.component.ts
+++ src/components/cosmetic-list/cosmetic-list.component.ts
@@ -11,6 +11,8 @@ import { CosmeticsService } from '../../services/cosmetics.service';
 import { CosmeticItemComponent } from '../cosmetic-item/cosmetic-item.component';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { Utils } from '../../util/utils';
+import { ShopEntry } from '../../model/cosmetics/shop.model';
+import { ShopEntryComponent } from '../shop-entry/shop-entry.component';
 
 @Component({
   selector: 'app-cosmetic-list',
@@ -21,7 +23,8 @@ import { Utils } from '../../util/utils';
     SearchBarComponent,
     CosmeticItemComponent,
     MatButtonModule,
-    MatIcon
+    MatIcon,
+    ShopEntryComponent
   ],
   templateUrl: './cosmetic-list.component.html',
   styleUrl: './cosmetic-list.component.scss'
@@ -36,6 +39,7 @@ export class CosmeticListComponent implements OnChanges {
   timeSub: Subscription = new Subscription; //Used to cycle images
   @Input() allCosmetics: Array<BrItem | Car | JamTrack | Instrument> = [];
   @Input() allAux: Array<BrItem | Car | JamTrack | Instrument> = [];
+  @Input() shopEntries?: Array<ShopEntry> = [];
   search: string | null = "";
   typeFilters: Gamemode[] = [Gamemode.BattleRoyale, Gamemode.Festival, Gamemode.RocketRacing];
   gamemode = Gamemode;
@@ -68,55 +72,19 @@ export class CosmeticListComponent implements OnChanges {
     this.timeSub.unsubscribe();
   }
 
-  getBrItemImages(item: BrItem | Cosmetic) {
-    let imgArray: Array<string | undefined> = [];
-    item.type.value == 'emoji' ? imgArray.push(item.images.smallIcon) : imgArray.push(item.images.icon);
-    let legoStyle = this.legoSkins.find(skin => skin.cosmeticId == item.id);
-    if (legoStyle) {
-      if (legoStyle.images && legoStyle.images.large) {
-        imgArray.push(legoStyle.images.large);
-      }
-    }
-    let bean = this.beans.find(skin => skin.cosmeticId == item.id);
-    if (bean) {
-      imgArray.push(bean.images.large);
-    }
-    return imgArray;
+  getBrItemImages(item: BrItem | Cosmetic, legoSkins: Array<LegoSkin>, beans: Array<Bean>) {
+    return Utils.getBrItemImages(item, legoSkins, beans);
   }
 
   //Check if the item is a 'BR item' bc we need to provide their
   //alternate images (lego or bean), or styles if present
   //other types of items don't have them
   isBrItem(item: BrItem | Car | Instrument | JamTrack) {
-    if (item.type && item.type.value) {
-      return item.type.value === 'outfit' || 
-        item.type.value === 'backpack' || 
-        item.type.value === 'pickaxe' || 
-        item.type.value === 'wrap' || 
-        item.type.value === 'emote' || 
-        item.type.value === 'glider' || 
-        item.type.value === 'contrail' || 
-        item.type.value === 'emoji' || 
-        item.type.value === 'loadingscreen' || 
-        item.type.value === 'spray' ||
-        item.type.value === 'shoe' || 
-        item.type.value === 'petcarrier' ||
-        item.type.value === 'pet'
-    } else return false;
+    return Utils.isBrItem(item);
   }
 
   getBrItemVariants(item: BrItem | Cosmetic) {
-    let variants: Array<string | undefined> = [];
-    if (item.variants) {
-      for (let channel of item.variants) {
-        for (let option of channel.options) {
-          variants.push(option.image);
-        }
-      }
-      return variants;
-    } else {
-      return [];
-    }
+    return Utils.getBrItemVariants(item);
   }
 
   //Different types of objects have different structures, so image
@@ -124,74 +92,11 @@ export class CosmeticListComponent implements OnChanges {
   //vary. By checking the type we can know how to provide the image
   //with its proper field name
   checkImageType(item: Cosmetic) {
-    let imgPath: string | undefined = '';
-    if (item.type) {
-      switch (item.type.value) {
-        case 'backpack':
-        case 'emote':
-        case 'loadingscreen':
-        case 'pickaxe':
-        case 'spray':
-        case 'wrap':
-        case 'outfit':
-        case 'contrail':
-        case 'glider':
-        case 'music':
-        case 'banner':
-        case 'aura':
-        case 'shoe':
-        case 'petcarrier':
-        case 'pet':
-          if (item.images.icon) {
-            imgPath = item.images.icon
-          } else {
-            imgPath = item.images.smallIcon;
-          }
-          break;
-
-        case 'emoji':
-          imgPath = item.images.smallIcon;
-          break;
-
-        //Car related cosmetics
-        case 'drifttrail':
-        case 'booster':
-        case 'skin':
-        case 'body':
-        case 'wheel':
-          if (item.images) {
-            if (item.images.large) {
-              imgPath = item.images.large;
-            } else {
-              imgPath = item.images.small;
-            }
-          }
-          break;
-
-        //Festival cosmetics
-        case 'mic':
-        case 'bass':
-        case 'guitar':
-        case 'keyboard':
-        case 'drum':
-          imgPath = item.images.large;
-          break;
-      }
-    } else {
-      return item.albumArt;
-    }
-    return imgPath;
+    return Utils.checkImageType(item);
   }
 
   getCosmeticType(item: Cosmetic): Type {
-    //jam tracks don't have the type property
-    if (!item.type) {
-      let type = new Type();
-      type.value = 'jamtrack';
-      type.displayValue = 'Jam Track';
-      return type;
-    }
-    else return item.type;
+    return Utils.getCosmeticType(item);
   }
 
   //Items from certain series might have a set of colors for a background gradient
--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,4 +1,4 @@
-import { Cosmetic } from "../model/cosmetics/cosmetic.model";
+import { Bean, BrItem, Car, Cosmetic, Instrument, JamTrack, LegoSkin, Type } from "../model/cosmetics/cosmetic.model";
 
 //Contains utility functions which are used in more than 1 component
 export class Utils {
@@ -25,5 +25,124 @@ export class Utils {
         } else {
             return '';
         }
+    }
+
+    static getBrItemVariants(item: BrItem | Cosmetic): Array<string | undefined> {
+        let variants: Array<string | undefined> = [];
+        if (item.variants) {
+            for (let channel of item.variants) {
+                for (let option of channel.options) {
+                    variants.push(option.image);
+                }
+            }
+            return variants;
+        } else {
+            return [];
+        }
+    }
+
+    static isBrItem(item: BrItem | Car | Instrument | JamTrack): boolean {
+        if (item.type && item.type.value) {
+            return item.type.value === 'outfit' ||
+                item.type.value === 'backpack' ||
+                item.type.value === 'pickaxe' ||
+                item.type.value === 'wrap' ||
+                item.type.value === 'emote' ||
+                item.type.value === 'glider' ||
+                item.type.value === 'contrail' ||
+                item.type.value === 'emoji' ||
+                item.type.value === 'loadingscreen' ||
+                item.type.value === 'spray' ||
+                item.type.value === 'shoe' ||
+                item.type.value === 'petcarrier' ||
+                item.type.value === 'pet'
+        } else return false;
+    }
+
+    static checkImageType(item: Cosmetic): string | undefined {
+        let imgPath: string | undefined = '';
+        if (item.type) {
+            switch (item.type.value) {
+                case 'backpack':
+                case 'emote':
+                case 'loadingscreen':
+                case 'pickaxe':
+                case 'spray':
+                case 'wrap':
+                case 'outfit':
+                case 'contrail':
+                case 'glider':
+                case 'music':
+                case 'banner':
+                case 'aura':
+                case 'shoe':
+                case 'petcarrier':
+                case 'pet':
+                    if (item.images.icon) {
+                        imgPath = item.images.icon
+                    } else {
+                        imgPath = item.images.smallIcon;
+                    }
+                    break;
+
+                case 'emoji':
+                    imgPath = item.images.smallIcon;
+                    break;
+
+                //Car related cosmetics
+                case 'drifttrail':
+                case 'booster':
+                case 'skin':
+                case 'body':
+                case 'wheel':
+                    if (item.images) {
+                        if (item.images.large) {
+                            imgPath = item.images.large;
+                        } else {
+                            imgPath = item.images.small;
+                        }
+                    }
+                    break;
+
+                //Festival cosmetics
+                case 'mic':
+                case 'bass':
+                case 'guitar':
+                case 'keyboard':
+                case 'drum':
+                    imgPath = item.images.large;
+                    break;
+            }
+        } else {
+            return item.albumArt;
+        }
+        return imgPath;
+    }
+
+    static getCosmeticType(item: Cosmetic): Type {
+        //jam tracks don't have the type property
+        if (!item.type) {
+            let type = new Type();
+            type.value = 'jamtrack';
+            type.displayValue = 'Jam Track';
+            return type;
+        }
+        else return item.type;
+    }
+
+    static getBrItemImages(item: BrItem | Cosmetic, legoSkins: Array<LegoSkin>, beans: Array<Bean>): Array<string | undefined> {
+        let imgArray: Array<string | undefined> = [];
+        item.type.value == 'emoji' ? imgArray.push(item.images.smallIcon) : imgArray.push(item.images.icon);
+        let legoStyle = legoSkins.find(skin => skin.cosmeticId == item.id);
+        if (legoStyle) {
+            if (legoStyle.images && legoStyle.images.large) {
+                imgArray.push(legoStyle.images.large);
+            }
+        }
+        let bean = beans.find(skin => skin.cosmeticId == item.id);
+        if (bean) {
+            imgArray.push(bean.images.large);
+        }
+        return imgArray;
     }
 }
